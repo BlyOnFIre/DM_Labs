5b0be3cca486361db710f5bf9b1ec266b478e65f
olegf <olegfreyak2002@gmail.com>
2023-04-22 15:28:04 +0200

    labs

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -5,40 +5,29 @@ import re
 parent = dict()
 rank = dict()
 
-
 # Function to open the input file
-def open_file():
-    try:
-        return open("l1_3.txt")
-    except FileNotFoundError:
-        print("Oops! File not exist...")
-        exit()
-
-    file_name = sys.argv[1]
+def open_file(file_name):
     try:
         return open(file_name)
     except FileNotFoundError:
         print("Oops! File not exist...")
-        exit()
+        sys.exit()
 
+# Function to create a set for a given vertex
+def make_set(vertex):
+    parent[vertex] = vertex
+    rank[vertex] = 0
 
-# Function to create a set for a given vertice
-def make_set(vertice):
-    parent[vertice] = vertice
-    rank[vertice] = 0
+# Function to find the root of a vertex
+def find(vertex):
+    if parent[vertex] != vertex:
+        parent[vertex] = find(parent[vertex])
+    return parent[vertex]
 
-
-# Function to find the root of a vertice
-def find(vertice):
-    if parent[vertice] != vertice:
-        parent[vertice] = find(parent[vertice])
-    return parent[vertice]
-
-
-# Function to merge two sets containing vertice1 and vertice2
-def union(vertice1, vertice2):
-    root1 = find(vertice1)
-    root2 = find(vertice2)
+# Function to merge two sets containing vertex1 and vertex2
+def union(vertex1, vertex2):
+    root1 = find(vertex1)
+    root2 = find(vertex2)
     if root1 != root2:
         if rank[root1] > rank[root2]:
             parent[root2] = root1
@@ -47,55 +36,66 @@ def union(vertice1, vertice2):
             if rank[root1] == rank[root2]:
                 rank[root2] += 1
 
+# Function to perform Boruvka's algorithm on a given graph
+def boruvka(graph):
+    for vertex in graph['vertices']:
+        make_set(vertex)
 
-# Function to perform Kruskal's algorithm on a given graph
-def kruskal(graph):
-    for vertice in graph['vertices']:
-        make_set(vertice)
+    min_spanning_tree = set()
+    num_of_components = len(graph['vertices'])
 
-    minimum_spanning_tree = set()
-    edges = list(graph['edges'])
-    edges.sort()
-    for edge in edges:
-        weight, vertice1, vertice2 = edge
-        if find(vertice1) != find(vertice2):
-            union(vertice1, vertice2)
-            minimum_spanning_tree.add(edge)
-    return minimum_spanning_tree
+    while num_of_components > 1:
+        cheapest = [-1] * len(graph['vertices'])
+        for edge in graph['edges']:
+            weight, vertex1, vertex2 = edge
+            root1 = find(vertex1)
+            root2 = find(vertex2)
+            if root1 != root2:
+                if cheapest[root1] == -1 or weight < cheapest[root1][0]:
+                    cheapest[root1] = edge
+                if cheapest[root2] == -1 or weight < cheapest[root2][0]:
+                    cheapest[root2] = edge
+
+        for edge in cheapest:
+            if edge != -1:
+                weight, vertex1, vertex2 = edge
+                root1 = find(vertex1)
+                root2 = find(vertex2)
+                if root1 != root2:
+                    union(vertex1, vertex2)
+                    min_spanning_tree.add((weight, vertex1, vertex2))
+                    num_of_components -= 1
+
+    return min_spanning_tree
 
 
-# Open the input file
-file = open_file()
+# Read the input file
+file_name = "l1_3.txt"
+file = open_file(file_name)
 
 # Read the size of the graph and create a list of vertices
 size = int(file.readline())
-vertices = []
-for i in range(size):
-    vertices.append(i)
-
-# Initialize the matrix and edges list
-matrix = dict()
-edges = []
+vertices = [i for i in range(size)]
 
 # Read the input file and store the edges in the edges list
+edges = []
 for line_index, line in enumerate(file):
     nodes = list(filter(None, re.split(r'\s', re.sub('\n', '', line))))
     for index, node in enumerate(nodes):
-        if line_index == index or node == '0' or (str(index) + ',' + str(line_index)) in matrix: continue
-        matrix[str(line_index) + ',' + str(index)] = 1
+        if line_index == index or node == '0' or (line_index, index) in edges: continue
         edges.append((int(node), line_index, index))
 
 # Create the graph with vertices and edges
 graph = {'vertices': vertices, 'edges': set(edges)}
 
-# Find the minimum spanning tree using Kruskal's algorithm
-min_spanning_tree = sorted(kruskal(graph))
+# Find the minimum spanning tree using Boruvka's algorithm
+min_spanning_tree = sorted(boruvka(graph))
 
 # Print the minimum spanning tree and its total weight
 print("Minimum spanning tree:")
 tree_weight = 0
-for node in min_spanning_tree:
-    tree_weight += node[0]
-    print(str(node[1]) + '->' + str(node[2]) + '(' + str(node[0]) + ')', end='; ')
+for edge in min_spanning_tree:
+    tree_weight += edge[0]
+    print(f"{edge[1]}->{edge[2]}({edge[0]})", end="; ")
 
 print("\nWeight of the minimum spanning tree:", tree_weight)
